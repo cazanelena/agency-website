58243129877e1ebe3584ea9e225f6bb11f142863
sgroil <laurencesgroi@hotmail.co.uk>
2023-05-22 08:57:15 +0100

    removed console log
    Relates #4

--- form.js
+++ form.js
@@ -3,6 +3,5 @@ const contactForm = document.getElementById("contactForm");
 const submitMessage = document.getElementById("submitMessage");
 
 contactForm.addEventListener("submit", function() {
-  console.log("hello")
   submitMessage.textContent = "Thanks for getting in touch with us. We aim to respond to all queries within 24 hours.";
   });
