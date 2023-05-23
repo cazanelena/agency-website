71acea02e250f3473eeec1b96c5f0ae627cd0aaf
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-05-23 17:55:03 +0200

    Added a button that take us up to the top of the page
    Relates #18

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-// Burger menu 
+// Burger menu
 const button = document.getElementById("burger");
 const div = document.querySelector(".openBurger");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   div.classList.toggle("openBurger");
-  button.classList.toggle('open');
+  button.classList.toggle("open");
 });
 
 // Contact button
 const contactButton = document.getElementById("contactButton");
-contactButton.addEventListener("click", function() {
+contactButton.addEventListener("click", function () {
   window.location.href = "form.html";
+});
+
+// Button to the top scroll
+const buttonTop = document.querySelector(".btn-top");
+const displayButton = () => {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      buttonTop.style.display = "block";
+    } else {
+      buttonTop.style.display = "none";
+    }
   });
+};
+
+const scrollToTop = () => {
+  buttonTop.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+};
+
+displayButton();
+scrollToTop();
